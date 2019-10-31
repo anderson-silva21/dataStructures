8e411419e65f9376c68db5d754660b49a5e6003b
Anderson S. Silva <andsoares2001@gmail.com>
2019-10-31 12:05:14 -0300

    Update pythonGraphics.py

--- pythonGraphics.py
+++ pythonGraphics.py
@@ -14,7 +14,7 @@ ax.legend() #adiciona legenda
 
 ax2.scatter(x, y2, label = 'Pontos')
 ax2.legend() #adiciona legenda do ax2
-#plt.show()
+#plt.show() não usamos show quando vamos salvar
 
 plt.tight_layout() #tira espaços extras da borda da imagem
 plt.savefig('/home/todos/alunos/ap/a2150875/Área de Trabalho/graficos.png') #Adicionar o endereço para salvar a imagem
