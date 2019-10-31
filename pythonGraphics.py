3c82433ba34199e01aa32c272fb0ff2ff952f794
Anderson S. Silva <andsoares2001@gmail.com>
2019-10-31 11:46:40 -0300

    Create pythonGraphics.py

---
+++ pythonGraphics.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+  
+x = np.arange(0, 50)
+y = np.random.randint(100, size = 50)
+y2 = np.random.randint(20, size = 50)
+
+fig, (ax, ax2) = plt.subplots(figsize=(8, 5), ncols = 2, nrows = 1)
+ax.plot(x, y, 'g--', label = 'Crescimento')
+#ax.set_xlabel('Eixo X')
+#ax.set_ylabel('Eixo Y')
+#ax.grid() #habilita o grid do grafico
+#ax.legend() #adiciona legenda
+
+ax2.scatter(x, y2, label = 'Pontos')
+#ax2.legend() #adiciona legenda do ax2
+#plt.show()
+
+plt.tight_layout() #tira espaços extras da borda da imagem
+plt.savefig('/home/todos/alunos/ap/a2150875/Área de Trabalho/graficos.png')
